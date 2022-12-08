7f94e95cd0ed2d0325f099daada1d683d0397caa
SUN-0207 <cuthanhson27@gmail.com>
2022-12-08 21:01:18 +0700

    fetch chuongtrinhhoc, sua tren thanh navbar

--- views/main/services/index.php
+++ views/main/services/index.php
@@ -2,31 +2,6 @@
 include_once('views/main/navbar.php');
 ?>
 <main id="main">
-  <!-- Modal -->
-  <?php
-      foreach ($products as $product) {
-        echo
-        '<div class="modal fade" id="exampleModal'. $product->id .'" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-scrollable">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">' . $product->name . '</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                  <img src="' . $product->img . '" class="card-img-top" alt="..." style="width: 70%; height=70%; margin-left: 15%;">
-                  <br></br>
-                  <h6 class="card-text"><strong>' . $product->description . '</strong></h6>
-                  <p>' . $product->content . '</p>
-                </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
-              </div>
-            </div>
-          </div>
-        </div>';
-      }
-    ?>
 
   <!-- ======= Breadcrumbs ======= -->
   <section id="breadcrumbs" class="breadcrumbs" style="background: #1E90FF;">
@@ -46,34 +21,65 @@ include_once('views/main/navbar.php');
   <!-- ======= Services Section ======= -->
   <section id="services" class="services">
     <div class="container">
-
-      <div class="section-title" data-aos="zoom-out">
-        <h2>Chương trình tiếng anh giao tiếp cơ bản</h2>
-      </div>
-
-      <div class="row">
-        <div class="col-lg-4 col-md-6">
-          <div class="card">
-            <img class="card-img-top" src="public/dist/img/logo-BK.png" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">BKEngrisk Quận 10</h5>
-              <p class="card-text">Địa chỉ: .. .</p>
+      <?php foreach($comments as $comment){
+        echo '
+          <div class="section-title mt-3" data-aos="zoom-out">
+            <h2>'. $comment->name .'</h2>
+            <h4>'. $comment->content .'</h4>
+            <div class="row">
+            <div class="col-lg-4 col-md-6">
+              <div class="card">
+                <img class="card-img-top p-2" src="public/dist/img/logo-BK.png" alt="Card image cap">
+                <div class="card-body">
+                  <h3>Khóa học TOEIC sơ cấp</h3>
+                  <h6 class="mb-2 text-muted">Giáo trình cân bằng 4 kỹ năng, các bài học thú vị</h6>
+                  <p class="bg-light text-dark p-3 mt-2 rounded">Thời lượng: 12 tuần</p>
+                  <p class="bg-light text-dark p-3 mt-2 rounded">Yêu cầu: Không</p>
+                  <p class="bg-light text-dark p-3 mt-2 rounded">
+                    <span>Số lượng: 30 người</span>
+                  </p>
+                  <p class="mt-2">Học phí: <span style="color: #00BFFF;font-size: 3rem;">10.000.000</span></p>
+                  <button type="button" class="btn btn-outline-info mt-3">XEM THÊM</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="card">
+                <img class="card-img-top p-2" src="public/dist/img/logo-BK.png" alt="Card image cap">
+                <div class="card-body">
+                  <h3>Khóa học TOEIC sơ cấp</h3>
+                  <h6 class="mb-2 text-muted">Giáo trình cân bằng 4 kỹ năng, các bài học thú vị</h6>
+                  <p class="bg-light text-dark p-3 mt-2 rounded">Thời lượng: 12 tuần</p>
+                  <p class="bg-light text-dark p-3 mt-2 rounded">Yêu cầu: Không</p>
+                  <p class="bg-light text-dark p-3 mt-2 rounded">
+                    <span>Số lượng: 30 người</span>
+                  </p>
+                  <p class="mt-2">Học phí: <span style="color: #00BFFF;font-size: 3rem;">10.000.000</span></p>
+                  <button type="button" class="btn btn-outline-info mt-3">XEM THÊM</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="card">
+                <img class="card-img-top p-2" src="public/dist/img/logo-BK.png" alt="Card image cap">
+                <div class="card-body">
+                  <h3>Khóa học TOEIC sơ cấp</h3>
+                  <h6 class="mb-2 text-muted">Giáo trình cân bằng 4 kỹ năng, các bài học thú vị</h6>
+                  <p class="bg-light text-dark p-3 mt-2 rounded">Thời lượng: 12 tuần</p>
+                  <p class="bg-light text-dark p-3 mt-2 rounded">Yêu cầu: Không</p>
+                  <p class="bg-light text-dark p-3 mt-2 rounded">
+                    <span>Số lượng: 30 người</span>
+                  </p>
+                  <p class="mt-2">Học phí: <span style="color: #00BFFF;font-size: 3rem;">10.000.000</span></p>
+                  <button type="button" class="btn btn-outline-info mt-3">XEM THÊM</button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
-          <div class="card">
-            <img class="card-img-top" src="public/dist/img/logo-BK.png" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">BKEngrisk Quận 2</h5>
-              <p class="card-text">Địa chỉ: ...</p>
-            </div>
-          </div>
-        </div>
-
-      </div>
+          </div>';
+      } ?>
   </section>
-  <section id="services" class="services">
+  <!-- <section id="services" class="services">
     <div class="container">
 
       <div class="section-title" data-aos="zoom-out">
@@ -101,40 +107,9 @@ include_once('views/main/navbar.php');
         </div>
 
       </div>
-  </section>
-  <section id="services" class="services">
-    <div class="container">
-
-      <div class="section-title" data-aos="zoom-out">
-        <h2>Chương trình học tiếng Anh từ xa</h2>
-      </div>
-
-      <div class="row">
-        <div class="col-lg-4 col-md-6">
-          <div class="card">
-            <img class="card-img-top" src="public/dist/img/logo-BK.png" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">BKEngrisk Quận 10</h5>
-              <p class="card-text">Địa chỉ: .. .</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
-          <div class="card">
-            <img class="card-img-top" src="public/dist/img/logo-BK.png" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">BKEngrisk Quận 2</h5>
-              <p class="card-text">Địa chỉ: ...</p>
-            </div>
-          </div>
-        </div>
-
-      </div>
-  </section>
-
-
-  <!-- <section id="services" class="services section-bg"> -->
-  <!-- <div class="container" data-aos="fade-up">
+  </section> -->
+  < <!-- <section id="services" class="services section-bg"> -->
+    <!-- <div class="container" data-aos="fade-up">
       <div class="section-title" data-aos="zoom-out">
         <h2>Sản phẩm</h2>
         <p>Các sản phẩm nổi bật</p>
